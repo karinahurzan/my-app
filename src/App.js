1416f4c2fb47cef7732276c22b12dcc9afa1cd89
hurzan.karina <kgurzan@gmail.com>
2025-05-21 17:15:27 +0000

    randomColor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/accordion';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
-      <Accordion />
+      {/* <Accordion /> */}
+      <RandomColor />
     </div>
   );
 }
