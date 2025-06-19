65dd3699a4536110c8c8dde779eda4a04676615c
karinahurzan <kgurzan@gmail.com>
2025-06-19 16:58:11 +0300

    search autocomplete

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TabTest from './components/custom-tabs/tab-test';
 import ModalTest from './components/custom-modal-popup/modal-test';
 import GithubProfileFinder from './components/github-profile-finder';
 import SearchAutocomplete from './components/search-autocomplete-with-api';
+import TicTacToe from './components/tic-tac-toe';
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
       <ModalTest />
       <GithubProfileFinder />
       <SearchAutocomplete />
+      <TicTacToe />
     </div>
   );
 }
